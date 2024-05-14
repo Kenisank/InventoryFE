2fd15ac53bc3d7061f17812b686be0540a98dfca
Kenechukwu Frank <kenisank1@gmail.com>
2024-05-14 23:15:07 +0100

    god

---
+++ src/app/components/auth/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MatButtonModule, MatCardModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+
+
+export class LoginComponent {
+
+}
